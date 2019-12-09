4996100439c189bde37ffafef55309252b31c994
Mateusz Łukasik <manfred.mateo@wp.pl>
2019-12-09 13:26:16 +0100

    initial configuration

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
