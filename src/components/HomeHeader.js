7da3a6f4e722c653dff0c426b7981c6decc2e091
Mateusz Łukasik <manfred.mateo@wp.pl>
2019-12-12 15:28:13 +0100

    Navbar i sekcja główna. Routing kieruje na widok logowania.

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,4 +1,11 @@
 import React, {Component} from 'react';
+import {
+    HashRouter,
+    Route,
+    Link,
+    Switch,
+    NavLink,
+} from 'react-router-dom';
 import './HomeHeader.scss';
 
 class HomeHeader extends Component {
@@ -10,10 +17,10 @@ class HomeHeader extends Component {
                     <div className="homeHeader">
                         <ul className="logReg">
                             <li className="logRegLi">
-                                <a href="" className="login">Zaloguj</a>
+                                <Link to="/login" className="login">Zaloguj</Link>
                             </li>
                             <li className="logRegLi">
-                                <a href="" className="register">Załóż konto</a>
+                                <Link to="/register" className="register">Załóż konto</Link>
                             </li>
                         </ul>
                         <ul className="nav">
--- src/components/HomeMain.js
+++ src/components/HomeMain.js
@@ -1,17 +1,33 @@
 import React, {Component} from 'react';
+import {
+    HashRouter,
+    Route,
+    Link,
+    Switch,
+    NavLink,
+} from 'react-router-dom';
 import './HomeMain.scss';
 
 class HomeMain extends Component {
-ntent
+    ntent
+
     render() {
-        return(
+        return (
             <div className="container main">
-                <div className="main_content">
-                    <p className="main_content_lead">Zacznij pomagać!<br/>Oddaj niechciane rzeczy w zaufane ręce.</p>
-                    <div className="main_content_dec"></div>
-                    <div className="main_content_widgets">
-                        <button className="main_btn_widget">Oddaj rzeczy</button>
-                        <button className="main_btn_widget">Zorganizuj zbiórkę</button>
+                <div className="main_left_div"></div>
+                <div className="main_right_div">
+                    <div className="main_content">
+                        <p className="main_content_lead">Zacznij pomagać!<br/>Oddaj niechciane rzeczy w zaufane ręce.
+                        </p>
+                        <div className="main_content_dec"></div>
+                        <div className="main_content_widgets">
+                            <Link to="./login">
+                                <button className="main_btn_widget left">Oddaj<br/>rzeczy</button>
+                            </Link>
+                            <Link to="./login">
+                                <button className="main_btn_widget right">Zorganizuj<br/>zbiórkę</button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
 
@@ -21,9 +37,6 @@ ntent
         )
     }
 }
-
-
-
 
 
 export default HomeMain;
