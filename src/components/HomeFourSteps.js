ef242c32e2702802b57a9d63b3d68c434669af08
Mateusz Łukasik <manfred.mateo@wp.pl>
2019-12-13 14:55:14 +0100

    poprawione stylowanie przycisku w sekcji 4steps

--- src/components/HomeFourSteps.js
+++ src/components/HomeFourSteps.js
@@ -36,7 +36,7 @@ class HomeFourSteps extends Component {
                         <p className="stepText">kurier przyjedzie<br/>w dogodnym terminie</p>
                     </div>
                 </div>
-                <Link to="./login">
+                <Link to="./login" className="link">
                     <button className="main_btn_widget">Oddaj<br/>rzeczy</button>
                 </Link>
             </div>
